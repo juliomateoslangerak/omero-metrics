895d3e208d4a35485efd724fd7580a54f8071cee
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-03-21 11:07:31 +0100

    update

---
+++ test/test_dash/test_dpd_demo.py
@@ -0,0 +1,31 @@
+import re
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_template_tag_use(client):
+    'Check use of template tag'
+
+    for name in ['demo-one', 'foi_key_measurement']:
+        url = reverse(name, kwargs={})
+
+        response = client.get(url)
+
+        assert response.content
+        assert response.status_code == 200
+
+        for src in re.findall('iframe src="(.*?)"', response.content.decode("utf-8")):
+            response = client.get(src + "_dash-layout")
+            assert response.status_code == 200, ""
+
+
+@pytest.mark.django_db
+def test_add_to_session(client):
+    'Check use of session variable access helper'
+
+    url = reverse('session-variable-example')
+    response = client.get(url)
+
+    assert response.content
+    assert response.status_code == 200
