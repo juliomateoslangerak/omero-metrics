1b856330b43f2e0aa7a08a7684240322a737bd97
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-05-07 10:42:48 +0200

    Change obj_type for file annotations to match schema and get_object func from OMERO

--- omero_metrics/tools/omero_tools.py
+++ omero_metrics/tools/omero_tools.py
@@ -137,7 +137,7 @@ def get_ref_from_object(obj) -> mm_schema.DataReference:
         case ImageWrapper():
             obj_type = "IMAGE"
         case FileAnnotationWrapper():
-            obj_type = "FILE"
+            obj_type = "FILEANNOTATION"
         case MapAnnotationWrapper():
             obj_type = "KEY_VALUES"
         case TagAnnotationWrapper():
