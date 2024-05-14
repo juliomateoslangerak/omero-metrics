e346258d3151b848f76a7ab8edd36c2fa2b9ebd0
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-05-14 15:46:09 +0200

    using interanal data from a yaml file to populate the graphs.

--- omero_metrics/dash_apps/dash_dataset_psf_beads.py
+++ omero_metrics/dash_apps/dash_dataset_psf_beads.py
@@ -110,19 +110,25 @@ def func_psf_callback(*args, **kwargs):
     image_o = kwargs['session_state']['image']
     image = image_o[0, 30, :, :, 0]
     stack = image_o[0, :, :, :, 0]
-    image_Z = px.imshow(np.max(stack, axis=0), zmin=image.min(), zmax=image.max(), color_continuous_scale="gray")
-    image_X = px.imshow(np.max(stack, axis=2), zmin=image.min(), zmax=image.max(), color_continuous_scale="gray")
-    image_Y = px.imshow(np.max(stack, axis=1), zmin=image.min(), zmax=image.max(), color_continuous_scale="gray")
+    image_Z = np.max(stack, axis=0)
+    image_X = np.max(stack, axis=2)
+    image_Y = np.max(stack, axis=1)
+    image_X = np.reshape(image_X, (image_X.shape[0], image_X.shape[1], 1))
+    image_Y = np.reshape(image_Y, (image_Y.shape[0], image_Y.shape[1], 1))
+    image_Z = np.reshape(image_Z, (image_Z.shape[0], image_Z.shape[1], 1))
+    image_X = 255*(image_X / image_X.max())
+    image_Y = 255*(image_Y / image_Y.max())
+    image_Z = 255*(image_Z / image_Z.max())
     bead_properties_df = kwargs['session_state']['bead_properties_df']
     bead_x_profiles_df = kwargs['session_state']['bead_x_profiles_df']
     bead_y_profiles_df = kwargs['session_state']['bead_y_profiles_df']
     bead_z_profiles_df = kwargs['session_state']['bead_z_profiles_df']
     fig1 = px.imshow(image, zmin=image.min(), zmax=image.max(), color_continuous_scale="gray")
-    #fig = make_subplots(rows=2, cols=2, specs=[[{}, {}], [{"colspan": 2}, None]],
-    #                    subplot_titles=("MIP X axis", "MIP Y axis", "MIP Z axis"))
-    fig = go.Figure().set_subplots(2, 2, horizontal_spacing=0.1)
-    fig = fig.add_trace(image_X, row=1, col=1)
-    fig = fig.add_trace(image_Y, row=1, col=2)
-    fig = fig.add_trace(image_Z, row=2, col=1)
-    fig = fig.update_layout(showlegend=False, title_text="Maximum Intensity Projection")
+    fig = make_subplots(rows=2, cols=2, specs=[[{}, {}], [{"colspan": 2}, None]],
+                        subplot_titles=("MIP X axis", "MIP Y axis", "MIP Z axis"))
+    #fig = go.Figure().set_subplots(2, 2, horizontal_spacing=0.1)
+    fig = fig.add_trace(go.Image(z=image_X, ), row=1, col=1)
+    fig = fig.add_trace(go.Image(z=image_Y), row=1, col=2)
+    fig = fig.add_trace(go.Image(z=image_Z), row=2, col=1)
+    fig = fig.update_layout(height=600, width=800)
     return fig1, fig, bead_properties_df.to_dict('records'), bead_x_profiles_df.to_dict('records'), bead_y_profiles_df.to_dict('records'), bead_z_profiles_df.to_dict('records')
