645a9bea7ba528e82b54218805ae5970ebc89800
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-06-14 16:42:01 +0200

    Refact THE TEMPLATE NAME

--- OMERO_metrics/views.py
+++ OMERO_metrics/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
 from omeroweb.webclient.decorators import (
     login_required,
-    render_response,
 )
 
-from .tools import load
-from .tools.data_managers import (
-    DatasetManager,
-)
-from .tools.data_preperation import (
-    get_dataset_ids_lists,
-    processed_data_project_view,
-)
 
 
 @login_required()
