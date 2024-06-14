ded54e352f450c303d88863aaeba67eb727daa4a
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-06-14 17:02:35 +0200

    Refact THE TEMPLATE NAME

--- OMERO-metrics/views.py
+++ OMERO-metrics/views.py
@@ -14,6 +14,6 @@ def index(request, conn=None, **kwargs):
     }
     return render(
         request,
-        "OMERO_metrics/index.html",
+        "OMERO-metrics/index.html",
         context,
     )
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="OMERO_metrics",
+    name="OMERO-metrics",
     version="0.1.0",
     description="A webapp to follow microscope performance over time",
     packages=find_packages(),
