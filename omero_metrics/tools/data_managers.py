a0fe94e62c12403503620db59d275b3662aad422
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-06-17 12:28:39 +0200

    Blacked

--- omero_metrics/tools/data_managers.py
+++ omero_metrics/tools/data_managers.py
@@ -2,12 +2,16 @@ import datetime
 import logging
 from typing import Union
 
-from microscopemetrics.samples import (
-    field_illumination,
-    psf_beads
+from microscopemetrics.samples import field_illumination, psf_beads
+from microscopemetrics_schema.datamodel import (
+    microscopemetrics_schema as mm_schema,
 )
-from microscopemetrics_schema.datamodel import microscopemetrics_schema as mm_schema
-from omero.gateway import BlitzGateway, DatasetWrapper, ImageWrapper, ProjectWrapper
+from omero.gateway import (
+    BlitzGateway,
+    DatasetWrapper,
+    ImageWrapper,
+    ProjectWrapper,
+)
 
 from omero_metrics.tools import load, dump, update, delete
 
@@ -20,12 +24,12 @@ ANALYSIS_MAPPINGS = {
 
 SAMPLE_MAPPINGS = {
     "FieldIllumination": field_illumination,
-    "PSFBeads": psf_beads
+    "PSFBeads": psf_beads,
 }
 
 DATASET_MAPPINGS = {
     "FieldIlluminationDataset": mm_schema.FieldIlluminationDataset,
-    "PSFBeadsDataset": mm_schema.PSFBeadsDataset
+    "PSFBeadsDataset": mm_schema.PSFBeadsDataset,
 }
 
 INPUT_MAPPINGS = {
@@ -49,14 +53,19 @@ class DatasetManager:
     It contains the data (microscope-metrics_schema datasets and dataset_collections) and the necessary methods
     to interact with OMERO and load and dump data.
     """
-    def __init__(self, conn: BlitzGateway, dataset: Union[DatasetWrapper, int]):
+
+    def __init__(
+        self, conn: BlitzGateway, dataset: Union[DatasetWrapper, int]
+    ):
         self._conn = conn
         if isinstance(dataset, DatasetWrapper):
             self.omero_dataset = dataset
         elif isinstance(dataset, int):
             self.omero_dataset = self._conn.getObject("Dataset", dataset)
         else:
-            raise ValueError("datasets must be a DatasetWrapper or a dataset id")
+            raise ValueError(
+                "datasets must be a DatasetWrapper or a dataset id"
+            )
 
         self.omero_project = self.omero_dataset.getParent()
         self.mm_dataset = None
@@ -77,13 +86,21 @@ class DatasetManager:
         if force_reload or self.mm_dataset is None:
             self.mm_dataset = load.load_dataset(self.omero_dataset)
         else:
-            raise NotImplementedError("partial loading of data from OMERO is not yet implemented")
+            raise NotImplementedError(
+                "partial loading of data from OMERO is not yet implemented"
+            )
 
     def load_analysis_config(self, force_reload=True):
-        if not force_reload and self.analysis_config and self.analysis_config_id:
+        if (
+            not force_reload
+            and self.analysis_config
+            and self.analysis_config_id
+        ):
             return
         else:
-            self.analysis_config_id, self.analysis_config = load.load_analysis_config(self.omero_project)
+            self.analysis_config_id, self.analysis_config = (
+                load.load_analysis_config(self.omero_project)
+            )
 
     def save_analysis_config(self):
         if not self.analysis_config:
@@ -95,9 +112,11 @@ class DatasetManager:
             new_key_values=self.analysis_config,
             target_key_values=self.analysis_config_id,
             replace=True,
-            new_description=f"config saved on {datetime.datetime.now()}"
+            new_description=f"config saved on {datetime.datetime.now()}",
         )
-        logger.info(f"Saved configuration on mapAnn id:{self.analysis_config_id}")
+        logger.info(
+            f"Saved configuration on mapAnn id:{self.analysis_config_id}"
+        )
 
     def _update_dataset_input_config(self, config):
         for key, val in config.items():
@@ -110,18 +129,26 @@ class DatasetManager:
             dump.dump_dataset(
                 conn=self._conn,
                 dataset=mm_ds,
-                target_project=self.omero_project
+                target_project=self.omero_project,
             )
 
     def process_data(self, force_reprocess=False):
         if not force_reprocess and self.is_processed():
             if self.is_validated():
-                logger.warning("Dataset has been processed and validated. Force reprocess to process again")
+                logger.warning(
+                    "Dataset has been processed and validated. Force reprocess to process again"
+                )
             else:
-                logger.warning("Dataset has been processed but not validated. Force reprocess to process again")
+                logger.warning(
+                    "Dataset has been processed but not validated. Force reprocess to process again"
+                )
             return False
         items_to_remove = []
-        config = {k:v for k, v in self.analysis_config.items() if k not in items_to_remove}
+        config = {
+            k: v
+            for k, v in self.analysis_config.items()
+            if k not in items_to_remove
+        }
 
         self._update_dataset_input_config(config)
         self.analysis_func(self.mm_dataset)
