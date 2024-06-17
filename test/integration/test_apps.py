1ff0664915e0293bd5843475baa5b85bdae46b31
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-06-17 13:56:13 +0200

    Refactoring the name again, from OMERO-metrics back to OMERO_metrics.

--- test/integration/test_apps.py
+++ test/integration/test_apps.py
@@ -29,7 +29,7 @@ class TestLoadIndexPage(IWebTest):
         conn = get_connection(user1)
         user_name = conn.getUser().getName()
         django_client = self.new_django_client(user_name, user_name)
-        index_url = reverse("metrics_index")
+        index_url = reverse("OMERO_metrics_index")
         # asserts we get a 200 response code etc
         rsp = get(django_client, index_url)
         html_str = rsp.content.decode()
