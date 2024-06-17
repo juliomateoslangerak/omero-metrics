b84442fe07686c74777031dc6a3374060b025fd2
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-06-17 15:25:33 +0200

    DELETING THINGS TO SEE IF THIS WILL WORK

--- setup.py
+++ setup.py
@@ -6,15 +6,4 @@ setup(
     description="A webapp to follow microscope performance over time",
     packages=find_packages(),
     keywords=["omero"],
-    install_requires=[
-        "pandas",
-        "django-plotly-dash",
-        "dpd_static_support",
-        "dash-bootstrap-components",
-        "django-bootstrap4",
-        "dash_mantine_components",
-        "dash-iconify",
-        "microscopemetrics_schema @ git+https://github.com/juliomateoslangerak/microscopemetrics-schema.git@dev",
-        "microscopemetrics @ git+https://github.com/juliomateoslangerak/microscope-metrics.git@dev",
-    ],
 )
