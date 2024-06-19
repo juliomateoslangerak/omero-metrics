a4fb66c59a7ab2ef25e556aa28de8f86cdb67e6a
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-06-19 10:36:17 +0200

    adding center plugin

--- OMERO_metrics/settings.py
+++ OMERO_metrics/settings.py
@@ -1,3 +1,10 @@
 from django.conf import settings
 
 settings.TOP_LINKS.append(["Metrics", "OMERO_metrics_index"])
+settings.CENTER_PLUGINS.append(
+    [
+        "Metrics View",
+        "OMERO_metrics_index/webclient_plugins/center_plugin.metricsview.js.html",
+        "metrics_view_panel",
+    ]
+)
