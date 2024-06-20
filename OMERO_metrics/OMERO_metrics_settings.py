2e0c4910fcc8fe0be017e079fb00112db33e9ac4
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-06-20 10:29:27 +0200

    Adding OMERO_metrics_settings.py

--- OMERO_metrics/OMERO_metrics_settings.py
+++ OMERO_metrics/OMERO_metrics_settings.py
@@ -3,7 +3,7 @@ from omeroweb.settings import process_custom_settings, report_settings
 
 
 OMERO_METRICS_SETTINGS_MAPPING = {
-    "omero.web.OMERO_metrics.top_links": [
+    "omero.web.ui.top_links": [
         "TOP_LINKS",
         [["Metrics", "OMERO_metrics_index"]],
     ],
