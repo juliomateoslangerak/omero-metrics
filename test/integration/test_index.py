01e69f6e9f80b183e179419d8e3765198d2c0bd9
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-06-28 17:50:38 +0200

    updating the test_index file

--- test/integration/test_index.py
+++ test/integration/test_index.py
@@ -17,13 +17,18 @@
 #
 #
 
-"""Integration tests for index page."""
+import yaml
 
+"""Integration tests for index page."""
+from omero.gateway import (
+    ImageWrapper
+)
 from omeroweb.testlib import IWebTest, get
 import pytest
 from django.urls import reverse
 from omero.gateway import BlitzGateway
-
+from django.shortcuts import resolve_url
+from structure_generator import generate_users_groups
 
 def get_connection(user, group_id=None):
     """Get a BlitzGateway connection for the given user's client."""
@@ -41,9 +46,19 @@ class TestLoadIndexPage(IWebTest):
     @pytest.fixture()
     def user1(self):
         """Return a new user in a read-annotate group."""
-        group = self.new_group(perms="rwra--")
-        user = self.new_client_and_user(group=group)
+        group = self.new_group(perms="rwrw--")
+        user = self.new_client_and_user(privileges=None)
         return user
+
+    @pytest.fixture(scope="session")
+    def server_structure(self):
+        with open("test/integration/server_structure.yaml", "r") as f:
+            server_structure = yaml.load(f, Loader=yaml.SafeLoader)
+        return server_structure
+
+    @pytest.fixture(scope="function")
+    def field_illumination_project(self, server_structure):
+        pass
 
     def test_load_index(self, user1):
         """Test loading the app home page."""
@@ -56,13 +71,17 @@ class TestLoadIndexPage(IWebTest):
         html_str = rsp.content.decode()
         assert "Welcome" in html_str
 
-    def test_load_group(self, user1):
-        """Test loading the app group page."""
+    def test_server_structure_load(self, server_structure):
+        assert server_structure["users"]["Abraracurcix"]["password"] == "abc123"
+
+    def test_image_wrapper(self, server_structure, user1):
         conn = get_connection(user1)
         user_name = conn.getUser().getName()
         django_client = self.new_django_client(user_name, user_name)
-        index_url = reverse("group")
-        # asserts we get a 200 response code etc
-        rsp = get(django_client, index_url)
-        html_str = rsp.content.decode()
-        assert "Microscoope" in html_str
+        generate_users_groups(conn, server_structure["users"], server_structure["microscopes"])
+        for g in conn.getGroupsMemberOf():
+            print("   ID:", g.getName(), " Name:", g.getId())
+        group = conn.getGroupFromContext()
+        print("Current group: ", group.getName())
+
+
