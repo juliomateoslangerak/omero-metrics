a2fb71dc7fe01ca6b7a6145545a1508c43d4677e
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-07-04 16:04:36 +0200

    Initializing gh-pages branch

---
+++ omero_metrics/tools/update.py
@@ -0,0 +1,34 @@
+from typing import Union
+
+from omero_metrics.tools import omero_tools
+import microscopemetrics_schema.datamodel as mm_schema
+from omero.gateway import (
+    BlitzGateway,
+    DatasetWrapper,
+    ImageWrapper,
+    ProjectWrapper,
+    FileAnnotationWrapper,
+    MapAnnotationWrapper,
+)
+
+
+def update_key_value(
+    conn: BlitzGateway,
+    new_key_values: Union[dict, mm_schema.KeyValues],
+    target_key_values: Union[int, MapAnnotationWrapper],
+    replace: bool,
+    new_name: str = None,
+    new_description: str = None,
+    new_namespace: str = None,
+):
+    if isinstance(target_key_values, int):
+        target_key_values = conn.getObject("MapAnnotation", target_key_values)
+
+    omero_tools.update_key_value(
+        annotation=target_key_values,
+        updated_annotation=new_key_values,
+        replace=replace,
+        annotation_name=new_name,
+        annotation_description=new_description,
+        namespace=new_namespace,
+    )
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="omero-metrics",
+    version="0.1.0",
+    description="A webapp to follow microscope performance over time",
+    packages=find_packages(),
+    keywords=["omero"],
+)
