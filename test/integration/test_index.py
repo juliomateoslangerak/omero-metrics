e334574e5936e6d3aafca62c9e53788a9ac2b6bb
wapaa <oumou.elmouminine.dhmine@insa.ueuromed.org>
2024-09-16 09:28:36 +0200

    formatting using black

--- test/integration/test_index.py
+++ test/integration/test_index.py
@@ -1,31 +1,11 @@
-#!/usr/bin/env python
-#
-# Copyright (c) 2024 University of Dundee.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-
 import yaml
 
 """Integration tests for index page."""
-from omero.gateway import ImageWrapper, ProjectWrapper
+from omero.gateway import ProjectWrapper
 from omeroweb.testlib import IWebTest, get
 import pytest
 from django.urls import reverse
 from omero.gateway import BlitzGateway
-from django.shortcuts import resolve_url
 
 
 def get_connection(user, group_id=None):
