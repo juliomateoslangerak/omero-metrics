3d7a09e65fa444dd275ef83b1cf2738ae154b670
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2025-11-07 14:26:07 +0100

    Fixed an issue whereby bead coordinates as floats where passed as indexes for the bead crops.

--- omero_metrics/dash_apps/dash_analyses/dash_psf_beads/dash_image_psf_beads.py
+++ omero_metrics/dash_apps/dash_analyses/dash_psf_beads/dash_image_psf_beads.py
@@ -443,7 +443,9 @@ def callback_mip(points, axis, channel_index, **kwargs):
             0, :, :, :, channel_index
         ]
         x0, xf, y0, yf = my_components.crop_bead_index(bead, min_dist, stack)
-        mip_x, mip_y, mip_z = my_components.mip_graphs(x0, xf, y0, yf, stack)
+        mip_x, mip_y, mip_z = my_components.mip_graphs(
+            int(x0), int(xf), int(y0), int(yf), stack
+        )
         fig_mip_go = my_components.fig_mip(mip_x, mip_y, mip_z)
         fig_mip_go.update_layout(
             coloraxis={
