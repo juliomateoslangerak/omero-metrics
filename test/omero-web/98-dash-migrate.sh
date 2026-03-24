56b8e65c3d5fb9cba611a6c9a176ff6b39c71b69
Oumou <oumoudhmine@gmail.com>
2026-03-24 22:37:33 +0100

    fix: run collectstatic on startup for Dash static assets
    Dash component JS bundles were not collected to STATIC_ROOT,
    causing 500 errors on all /static/dash/component/ requests.

--- test/omero-web/98-dash-migrate.sh
+++ test/omero-web/98-dash-migrate.sh
@@ -5,4 +5,7 @@ python=/opt/omero/web/venv3/bin/python
 cd /opt/omero/web/venv3/lib/python3.10/site-packages/omeroweb
 
 echo "Run migrate to update the database OMERO.web"
-exec $python manage.py migrate
+$python manage.py migrate
+
+echo "Collecting static files for OMERO.web"
+$python manage.py collectstatic --noinput
